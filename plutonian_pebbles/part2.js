191195296d1e06165d6327d5b70fc3367df217f1
Darnell <darnjames@gmail.com>
2024-12-18 22:51:35 -0500

    Solve day 11

--- plutonian_pebbles/part2.js
+++ plutonian_pebbles/part2.js
@@ -2,66 +2,66 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf8").split(" ");
 
 function blinkStone(stones) {
-  // convert input to linked list
-  const list = createList(stones);
+  let rocks = createMap(stones);
   let rockCount = 0;
-
   const blinks = 75;
-  for (let i = 0; i < blinks; i++) {
-    let curr = list;
 
-    while (curr) {
-      if (curr.value === "0") {
-        curr.value = "1";
-      } else if (curr.value.length % 2 === 0) {
-        const half = curr.value.length / 2;
+  for (let i = 0; i < blinks; i++) {
+    const newRocks = [];
+    for (let r in rocks) {
+      if (r === "0") {
+        newRocks.push(["1", rocks[r]]);
+      } else if (r.length % 2 === 0) {
+        const half = r.length / 2;
         const left = [];
         const right = [];
         let c = 0;
         while (c < half) {
-          left.push(curr.value[c]);
+          left.push(r[c]);
           c += 1;
         }
 
-        while (curr.value[c] === "0" && c !== curr.value.length - 1) {
+        while (r[c] === "0" && c !== r.length - 1) {
           c += 1;
         }
 
-        while (c < curr.value.length) {
-          right.push(curr.value[c]);
+        while (c < r.length) {
+          right.push(r[c]);
           c += 1;
         }
 
-        curr.value = left.join("");
-        const moveRight = curr.next;
-        const next = { value: right.join(""), next: moveRight };
-        curr.next = next;
-        curr = curr.next;
+        const leftStr = left.join("");
+        const rightStr = right.join("");
+        newRocks.push([leftStr, rocks[r]]);
+        newRocks.push([rightStr, rocks[r]]);
       } else {
-        curr.value = (Number(curr.value) * 2024).toString();
+        const prod = (Number(r) * 2024).toString();
+        newRocks.push([prod, rocks[r]]);
       }
-      curr = curr.next;
     }
+
+    const nextBlink = {};
+
+    for (let rock of newRocks) {
+      nextBlink[rock[0]] = nextBlink[rock[0]] + rock[1] || rock[1];
+    }
+
+    rocks = nextBlink;
   }
 
-  let currRock = list;
-  while (currRock) {
-    rockCount += 1;
-    currRock = currRock.next;
+  for (let k in rocks) {
+    rockCount += rocks[k];
   }
 
   return rockCount;
 }
 
-function createList(input) {
-  const list = { value: input[0], next: null };
-  let curr = list;
-
-  for (let i = 1; i < input.length; i++) {
-    curr.next = { value: input[i], next: null };
-    curr = curr.next;
+function createMap(input) {
+  const map = {};
+  for (let i = 0; i < input.length; i++) {
+    map[input[i]] = 1;
   }
-  return list;
+  return map;
 }
 
 console.time("blinkStone");
--- plutonian_pebbles/temp.md
+++ plutonian_pebbles/temp.md
@@ -21,3 +21,27 @@ Be careful with mathematical operations, since all of the values are strings. Fo
 
 - Turn your input into a linked list. With a list, it'll be easier to insert items into the list of rocks.
 - Loop through the linked list X amount of times. Each time you do, apply the rule to the list of rocks.
+
+## Part 2
+
+The part 1 approach runs out of memory.
+Try to use an object that keeps track of the number of number instances.
+So instead of making new strings, just count the number of strings. So maybe don't create a linked list. Just use a map
+
+Maybe do two objects? Here me out.
+One object holds the count of the specific numbered rock
+While the other object just holds the result of that rock so that you don't have to do the computation over again.
+If a stone numbered 2 produces X results after Y blinks, another stone numbered 2 will do the same, so there's no reason to calc it.
+
+Store the result of each stone and the number of blinks
+Have another object store the number of stones. Might be a little weird due to stones being replaced at times.
+Also, what do you loop through? No linked list. Are you going to loop through the object and fetch keys?
+I think a DFS is the way to go, man. Loop through the stone and perform a DFS on each number for the number of blinks
+If a number is replaced, don't increment the number of stones. If it splits, increment the number by 1.
+
+Try doing the memoization approach. Cache the stone number and number of blinks, saying how many stones you'd have after X blinks.
+
+So here's the plan
+Loop through the array of rocks and place any created rocks into the map, storing their number.
+No duplicates get sorted at all. Duplicates just add to the amount of rocks for that number.
+When the number splits, the new number takes the amount that the previous number had.
